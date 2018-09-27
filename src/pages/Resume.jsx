62eb8e81d39c2d3595b87cca0f2799bf83fab1dd
Marvin Cangcianno <mcangcianno3@gatech.edu>
2018-09-26 23:23:12 -0400

    add resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Grid, Row } from 'react-bootstrap';
-// import './../styles/Contact.css';
+import './../styles/Resume.css';
 
 class Contact extends React.Component {
   render() {
     return (
       <Grid>
         <Row className="items">
-          RESUME
+          <p>Last updated on 09/24/2018</p>
+          <object className="resume" data="./MarvinCangciannoResume.pdf" type="application/pdf" />
         </Row>
       </Grid>
     );
